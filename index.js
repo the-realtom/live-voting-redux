a5d4ec961cab47c9265fdeade11b549881af589d
the-realtom <thomas.white@connect.qut.edu.au>
2017-02-28 21:35:38 +1000

    added documentation

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import makeStore from './src/store';
 import startServer from './src/server';
 
+/* Give startServer the REDUX store so the socket.io can
+ * subscribe and emit state changes
+* */
 export const store = makeStore();
 startServer(store);
 
