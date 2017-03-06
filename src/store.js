e52b1ebb1236e3bf12877f5cfb7fcdf00dc818b4
the-realtom <thomas.white@connect.qut.edu.au>
2017-03-06 22:27:53 +1000

    'documentation'

--- src/store.js
+++ src/store.js
@@ -1,6 +1,8 @@
 import {createStore} from 'redux';
 import reducer from './reducer';
 
+/* Creates a Redux store with a reducer consisting of three actions
+* */
 export default function makeStore() {
     return createStore(reducer);
 }
